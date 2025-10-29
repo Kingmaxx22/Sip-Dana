ab27323c3ff5c07c97fc001d321dcf6f336e7b1b
ryhnnas <agrahans13@gmail.com>
2025-10-29 12:55:43 +0700

    Menambahkan Api di ubuntu

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: 'https://api.ryhnnas.web.id/sipdana/api',
 });
 
 api.interceptors.request.use(
